81f5299e804b25417ae1bc7cc3bf1946d2758fb7
Govindarajulu K <285203866+govinda-rajulu@users.noreply.github.com>
2026-08-06 04:35:52 +0000

    morphe.sh: YouTube arm64-v8a only

--- src/build/morphe.sh
+++ src/build/morphe.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
 # Morphe build
 source ./src/build/utils.sh
-
 # Download requirements
 morphe_dl(){
-	dl_gh "morphe-patches" "MorpheApp" "latest"
-	dl_gh "morphe-desktop" "MorpheApp" "latest"
+ dl_gh "morphe-patches" "MorpheApp" "latest"
+ dl_gh "morphe-desktop" "MorpheApp" "latest"
 }
-
 1() {
-	morphe_dl
-	# Patch YouTube arm64-v8a only:
-	get_patches_key "youtube-morphe"
-	get_apk "com.google.android.youtube" "youtube" "apk" "arm64-v8a"
-	patch "youtube" "morphe"
+ morphe_dl
+ # Patch YouTube:
+ get_patches_key "youtube-morphe"
+ get_apk "com.google.android.youtube" "youtube" "apk"
+ patch "youtube" "morphe"
+ # Split to arm64-v8a only (archs[0])
+ for i in 0; do
+ split_arch "youtube" "morphe"
+ done
 }
-
-2() {
-	# Placeholder - no-op for matrix run=2
-	:
-}
-
-3() {
-	# Placeholder - no-op for matrix run=3
-	:
-}
-
 case "$1" in
-    1)
-        1
-        ;;
-    2)
-        2
-        ;;
-    3)
-        3
-        ;;
+ 1)
+ 1
+ ;;
 esac
